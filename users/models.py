6eda3440cb06f7cb8463f0a8488d2c55fb3996cd
Maksym <maxim03102007@gmail.com>
2025-10-11 18:21:55 +0200

    user model

---
+++ users/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.html import strip_tags
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, first_name, last_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email must be set.')
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(email=email, username=username, first_name=first_name,
+                           lastname=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_super_user(self, email, username, first_name, last_name, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(username, email, first_name, last_name, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, max_length=66)
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=66)
+    last_name = models.CharField(max_length=66)
+    city = models.CharField(max_length=128, blank=True, null=True)
+    country = models.CharField(max_length=128, blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+    
+    def clean(self):
+        for field in ['city', 'country', 'phone']:
+            value = getattr(self, field)
+            if value:
+                setattr(self, field, strip_tags(value))
