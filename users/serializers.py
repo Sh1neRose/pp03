3bf88b064d0dbcceac2140925727841dafe508ae
Максим <maxim03102007@gmail.com>
2025-10-24 19:12:04 +0200

    first time used serializers, so i did UserSerializer, UserCreateSerializer for create user and UserLoginSerializer for login user

---
+++ users/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model, authenticate
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name',]
+        read_only_fields = ['id']
+
+class UserCreateSerializer(UserSerializer):
+    password1 = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
+
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + ['password1', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password1']!=attrs['password2']:
+            raise serializers.ValidationError({'password': 'Пароль не совпадает'})
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        password = validated_data.pop('password1')
+        user = User.objects.create_user(password=password, **validated_data)
+        return user
+    
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(username=email, password=password)
+        if not user:
+            raise serializers.ValidationError('Не верные данные')
+        attrs['user'] = user
+        return attrs
