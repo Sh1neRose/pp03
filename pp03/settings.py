0b6771f91a25a7e1cf7e3978d0a8c088f3b2c34d
Максим <maxim03102007@gmail.com>
2025-10-27 19:36:56 +0100

    added settings for email and celery

--- pp03/settings.py
+++ pp03/settings.py
@@ -155,3 +155,15 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
+
+# Celery settings
+CELERY_BROKER_URL = env('CELERY_BROKER_URL')
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS', default=False)
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='default@localhost')
